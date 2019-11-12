7d1cfc4020d1713ec0b943044c606653caa8488f
Peter Getov <peter.getov@gmail.com>
2019-11-12 19:34:57 +0200

    * updated russian localization

--- Localization/Localization_ru.lua
+++ Localization/Localization_ru.lua
@@ -3,4 +3,4 @@ if GetLocale() ~= "ruRU" then return end
 -- Translate the strings here
 CSC_DEFENSE                 = DEFENSE;
 CSC_WEAPON_SKILLS_HEADER    = "Оружейные навыки";
-CSC_HIT_BIZNICKS_TXT        = HIT;
+CSC_HIT_BIZNICKS_TXT        = "к рейтингу меткости";
