7b4b8a0095902121e38ee0851d40a306edae86cc
Peter Getov <peter.getov@gmail.com>
2020-03-14 13:46:37 +0200

    Hopefully fixed the multiple block value count of the Battlegear of Might set.

--- CharacterStatsClassicUtils.lua
+++ CharacterStatsClassicUtils.lua
@@ -743,11 +743,22 @@ end
 local function CSC_GetBlockValue(unit)
 	CSC_ScanTooltip:ClearLines();
 
-	local blockFromShield = 0;
+	local blockValueFromItems = 0;
 	local firstItemslotIndex = 1;
 	local lastItemslotIndex = 18;
 
 	local blockValueIDs = { ITEM_MOD_BLOCK_RATING_SHORT, ITEM_MOD_BLOCK_RATING, ITEM_MOD_BLOCK_VALUE };
+
+	local equippedMightSetItems = 0;
+	local battlegearOfMightIDs = { [16861] = 16861, 
+								   [16862] = 16862, 
+								   [16863] = 16863, 
+								   [16864] = 16864, 
+								   [16865] = 16865, 
+								   [16866] = 16866, 
+								   [16867] = 16867, 
+								   [16868] = 16868
+								};
 
 	for itemslot=firstItemslotIndex, lastItemslotIndex do
 		local hasItem = CSC_ScanTooltip:SetInventoryItem(unit, itemslot);
@@ -756,17 +767,22 @@ local function CSC_GetBlockValue(unit)
 			for line=1, maxLines do
 				local leftText = getglobal(CSC_ScanTooltipPrefix.."TextLeft"..line);
 				if leftText:GetText() then
-					for blockValueID=1, 3 do
-						local valueTxt = string.match(leftText:GetText(), "%d+ "..blockValueIDs[blockValueID]);
-						if not valueTxt then
-							valueTxt = string.match(leftText:GetText(), string.sub( blockValueIDs[blockValueID], 1, -5).." %d+");
-						end
-						if valueTxt then
-							valueTxt = string.match(valueTxt, "%d+");
+					local itemId = GetInventoryItemID(unit, itemslot);
+					if (itemId == battlegearOfMightIDs[itemId]) then
+						equippedMightSetItems = equippedMightSetItems + 1;
+					else
+						for blockValueID=1, 3 do
+							local valueTxt = string.match(leftText:GetText(), "%d+ "..blockValueIDs[blockValueID]);
+							if not valueTxt then
+								valueTxt = string.match(leftText:GetText(), string.sub( blockValueIDs[blockValueID], 1, -5).." %d+");
+							end
 							if valueTxt then
-								local numValue = tonumber(valueTxt);
-								if numValue then
-									blockFromShield = blockFromShield+numValue;
+								valueTxt = string.match(valueTxt, "%d+");
+								if valueTxt then
+									local numValue = tonumber(valueTxt);
+									if numValue then
+										blockValueFromItems = blockValueFromItems + numValue;
+									end
 								end
 							end
 						end
@@ -778,7 +794,12 @@ local function CSC_GetBlockValue(unit)
 
 	local strStatIndex = 1;
 	local strength = select(2, UnitStat(unit, strStatIndex));
-	local blockValue = blockFromShield + (strength / 20);
+	local blockValue = blockValueFromItems + (strength / 20);
+	
+	local requiredMightSetItems = 3;
+	if (equippedMightSetItems == requiredMightSetItems) then
+		blockValue = blockValue + 30; -- Set bonus reached
+	end
 
 	return blockValue;
 end
