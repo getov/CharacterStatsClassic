0cfb9d5c5b8c5844b8e05355b81ef693ba527dc8
chkid <55096941+chkmyid@users.noreply.github.com>
2020-05-07 21:45:12 +0900

    Update Localization_kr.lua

--- Localization/Localization_kr.lua
+++ Localization/Localization_kr.lua
@@ -15,10 +15,10 @@ CSC_WEAPON_UNARMED_TXT      = "맨손 전투";
 CSC_WEAPON_DAGGER_TXT       = "단검류";
 CSC_HIT_BIZNICKS_TXT        = HIT;
 
-CSC_SPELL_HIT_TOOLTIP_TXT       = "Spell Hit chance from gear: %d%%";
-CSC_SPELL_HIT_SUBTOOLTIP_TXT    = "Spell Hit chance (Gear + Talents):";
-CSC_ARCANE_SPELL_HIT_TXT        = "Arcane Spell Hit";
-CSC_FIRE_SPELL_HIT_TXT          = "Fire Spell Hit";
-CSC_FROST_SPELL_HIT_TXT         = "Frost Spell Hit";
-CSC_DESTRUCTION_SPELL_HIT_TXT   = "Destruction Spell Hit";
-CSC_AFFLICTION_SPELL_HIT_TXT    = "Affliction Spell Hit";
+CSC_SPELL_HIT_TOOLTIP_TXT       = "장비에 의한 주문 적중률: %d%%";
+CSC_SPELL_HIT_SUBTOOLTIP_TXT    = "주문 적중률 (장비 + 특성):";
+CSC_ARCANE_SPELL_HIT_TXT        = "비전 주문 적중";
+CSC_FIRE_SPELL_HIT_TXT          = "화염 주문 적중";
+CSC_FROST_SPELL_HIT_TXT         = "냉기 주문 적중";
+CSC_DESTRUCTION_SPELL_HIT_TXT   = "파괴 주문 적중";
+CSC_AFFLICTION_SPELL_HIT_TXT    = "고통 주문 적중";
