e959c9c67fd5b045b158b5b0d197b751b8b07c81
Peter Getov <peter.getov@gmail.com>
2020-07-12 16:57:51 +0300

    Updated zhCN and zhTW localizations

--- Localization/Localization_cn.lua
+++ Localization/Localization_cn.lua
@@ -15,10 +15,10 @@ CSC_WEAPON_UNARMED_TXT      = "拳套";
 CSC_WEAPON_DAGGER_TXT       = "匕首";
 CSC_HIT_BIZNICKS_TXT        = HIT;
 
-CSC_SPELL_HIT_TOOLTIP_TXT       = "Spell Hit chance from gear: %d%%";
-CSC_SPELL_HIT_SUBTOOLTIP_TXT    = "Spell Hit chance (Gear + Talents):";
-CSC_ARCANE_SPELL_HIT_TXT        = "Arcane Spell Hit";
-CSC_FIRE_SPELL_HIT_TXT          = "Fire Spell Hit";
-CSC_FROST_SPELL_HIT_TXT         = "Frost Spell Hit";
-CSC_DESTRUCTION_SPELL_HIT_TXT   = "Destruction Spell Hit";
-CSC_AFFLICTION_SPELL_HIT_TXT    = "Affliction Spell Hit";
+CSC_SPELL_HIT_TOOLTIP_TXT       = "法术命中（装备）: %d%%";
+CSC_SPELL_HIT_SUBTOOLTIP_TXT    = "法术命中（装备+天赋）:";
+CSC_ARCANE_SPELL_HIT_TXT        = "奥术法术命中";
+CSC_FIRE_SPELL_HIT_TXT          = "火焰法术命中";
+CSC_FROST_SPELL_HIT_TXT         = "冰霜法术命中";
+CSC_DESTRUCTION_SPELL_HIT_TXT   = "毁灭法术命中";
+CSC_AFFLICTION_SPELL_HIT_TXT    = "痛苦法术命中";
--- Localization/Localization_tw.lua
+++ Localization/Localization_tw.lua
@@ -15,10 +15,10 @@ CSC_WEAPON_UNARMED_TXT      = "拳套";
 CSC_WEAPON_DAGGER_TXT       = "匕首";
 CSC_HIT_BIZNICKS_TXT        = HIT;
 
-CSC_SPELL_HIT_TOOLTIP_TXT       = "Spell Hit chance from gear: %d%%";
-CSC_SPELL_HIT_SUBTOOLTIP_TXT    = "Spell Hit chance (Gear + Talents):";
-CSC_ARCANE_SPELL_HIT_TXT        = "Arcane Spell Hit";
-CSC_FIRE_SPELL_HIT_TXT          = "Fire Spell Hit";
-CSC_FROST_SPELL_HIT_TXT         = "Frost Spell Hit";
-CSC_DESTRUCTION_SPELL_HIT_TXT   = "Destruction Spell Hit";
-CSC_AFFLICTION_SPELL_HIT_TXT    = "Affliction Spell Hit";
+CSC_SPELL_HIT_TOOLTIP_TXT       = "法術命中（裝備）: %d%%";
+CSC_SPELL_HIT_SUBTOOLTIP_TXT    = "法術命中（裝備+天賦）:";
+CSC_ARCANE_SPELL_HIT_TXT        = "奧術法術命中";
+CSC_FIRE_SPELL_HIT_TXT          = "火焰法術命中";
+CSC_FROST_SPELL_HIT_TXT         = "冰霜法術命中";
+CSC_DESTRUCTION_SPELL_HIT_TXT   = "毀滅法術命中";
+CSC_AFFLICTION_SPELL_HIT_TXT    = "痛苦法術命中";
